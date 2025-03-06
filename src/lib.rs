c2b9a63305e4c90b327bb90425fadda492f78f2e
huanshang141 <1375436987@qq.com>
2025-03-06 15:14:09 +0800

    finish read_and_print

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::thread;
+use std::{fs, thread};
+use std::path::Path;
 use std::time::Duration;
 
 pub fn count_down(second:u64){
@@ -8,12 +9,29 @@ pub fn count_down(second:u64){
     }
     println!("Countdown finished!");
 }
+
+pub fn read_and_print(file_path: &str){
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join(file_path);
+    let contents = fs::read_to_string(path).expect("File not found!");
+    println!("{}", contents);
+}
+
 #[cfg(test)]
-mod tests {
+mod test_count_down {
     use crate::count_down;
 
     #[test]
     fn count_down_test() {
         count_down(1);
+    }
+}
+
+#[cfg(test)]
+mod test_read_and_print {
+    use crate::read_and_print;
+
+    #[test]
+    fn read_and_print_test(){
+        read_and_print("test.txt");
     }
 }
